b8f518abf2a65a136c0b9bae17e83cbfe9ae6451
Maizhihui <879774724@qq.com>
2023-08-20 13:15:50 +0800

    Update script.js
    文件注释调整

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 window.onload = function () {
-  // ファイル操作関連
+  //文件操作相关
   const fileInput = document.getElementById('upload');
   const uploadBtn = document.getElementById('uploadBtn');
   const downloadBtn = document.getElementById('downloadBtn');
 
-  // ローディング関連
+  //加载相关
   const loadingDiv = document.getElementById('loading');
 
-  // サンプル画像関連
+  //样例图像
   const sampleContent = document.getElementById('sampleContent');
   const slideShowImage = document.getElementById('slideshowImage');
 
-  // 結果画像表示関連
+  //结果图像显示相关
   const contentsDiv = document.getElementById('resultContents');
   const resultImage = document.getElementById('resultImage');
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
-  // 結果表示・修正フォーム関連
+  //结果显示和更正表格相关
   const makeInput = document.getElementById('makeInput');
   const modelInput = document.getElementById('modelInput');
   const lensInput = document.getElementById('lensInput');
@@ -37,8 +37,7 @@ window.onload = function () {
   });
 
   fileInput.addEventListener('change', function (e) {
-
-    // サンプルコンテンツを非表示にしてローディング画面を表示
+    //隐藏样例内容并显示加载屏幕
     sampleContent.style.display = 'none';
     toggleImageDisplay();
     toggleLoading();
@@ -49,11 +48,9 @@ window.onload = function () {
     reader.onloadend = function () {
       let img = new Image();
       img.onload = function () {
-        // 修正用に元画像を変数に保存
+        //将原始图像保存到变量以进行修改
         imgData = img;
-        // EXIF情報を取得
         let exifData = {};
-        // レンズ情報を取得するためにExif.jsのタグを追加
         EXIF.Tags[0xA432]="LensSpecification";
         EXIF.Tags[0xA433]="LensMake";
         EXIF.Tags[0xA434]="LensModel";
@@ -62,7 +59,7 @@ window.onload = function () {
           exifData = EXIF.getAllTags(this);
         });
 
-        // フォント読み込みとテキストの描画（フォントの二重読み込み防止処理）
+        //字体加载和文本渲染（防止字体双重加载）
         if (isFontsLoaded) {
           draw(exifData)
         } else {
@@ -79,14 +76,13 @@ window.onload = function () {
       reader.readAsDataURL(file);
     }
 
-    // ダウンロードボタンを有効化
+    //启用下载按钮
     downloadBtn.disabled = false;
   }, false);
 
   downloadBtn.addEventListener('click', function () {
-    // aタグを作成してclickイベントを発生させることで、キャンバス内容を画像としてダウンロード
     let a = document.createElement('a');
-    a.href = canvas.toDataURL('image/jpeg');
+    a.href = canvas.toDataURL('image/jpeg', 1.0);
     let fileNameWithoutExtension = file.name.split('.')[0];
     a.download = fileNameWithoutExtension + '-frame.jpeg';
     a.click();
@@ -115,9 +111,8 @@ window.onload = function () {
     draw(exifData)
   });
 
-  // サンプル画像のスライドショー
   function slideshowtimer() {
-    if (slideNum === 1) {
+    if (slideNum === 3) {
       slideNum = 0;
     }
     else {
@@ -130,7 +125,6 @@ window.onload = function () {
   setInterval(slideshowtimer, 3000);
 
   function loadFonts() {
-    // CSS Font Loading APIを使用してフォントを読み込む
     const fontInter400 = new FontFace('Inter', 'url(./fonts/inter-v12-latin-regular.woff2)', { weight: '400' });
     const fontInter500 = new FontFace('Inter', 'url(./fonts/inter-v12-latin-500.woff2)', { weight: '500' });
     const fontInter700 = new FontFace('Inter', 'url(./fonts/inter-v12-latin-700.woff2)', { weight: '700' });
@@ -142,7 +136,7 @@ window.onload = function () {
 
       isFontsLoaded = true;
     }).catch(function (error) {
-      alert('フォントの読み込みに失敗しました: ' + error);
+      alert('无法加载字体：' + error);
     });
   }
 
@@ -152,35 +146,28 @@ window.onload = function () {
     let text3 = '';
     if( isMobile() ){
       //调整尺寸
-      alert('移动端');
       imgData.width = imgData.width * 0.70;
       imgData.height = imgData.height * 0.70;
     }
-    // 画像に応じたマージンやフォントサイズを計算
-    const HORIZONTAL_MARGIN = imgData.width * 0.025;  // 余白の大きさ
-    const BOTTOM_MARGIN = imgData.width > imgData.height ? imgData.height * 0.25 : imgData.width * 0.17;  // 下部の余白の大きさ
-    const BASE_FONT_SIZE = imgData.width > imgData.height ? imgData.height * 0.0275 : imgData.width * 0.02  // ベースとなるフォントサイズ
-    const FONT_FAMILY = 'Inter, sans-serif' // フォント
-    const LINE_SPACING = imgData.width > imgData.height ? imgData.height * 0.005 : imgData.width * 0.0045;  // 行間
+    const HORIZONTAL_MARGIN = imgData.width * 0.025;
+    const BOTTOM_MARGIN = imgData.width > imgData.height ? imgData.height * 0.25 : imgData.width * 0.17;
+    const BASE_FONT_SIZE = imgData.width > imgData.height ? imgData.height * 0.0275 : imgData.width * 0.02;
+    const FONT_FAMILY = 'Inter, sans-serif';
+    const LINE_SPACING = imgData.width > imgData.height ? imgData.height * 0.005 : imgData.width * 0.0045;
 
-    // キャンバスサイズを画像サイズ＋枠分に設定
     canvas.width = imgData.width + HORIZONTAL_MARGIN * 2;
     canvas.height = imgData.height + BOTTOM_MARGIN;
-    // 白い背景を描画
     ctx.fillStyle = '#ffffff';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // 画像を描画（余白の分だけずらして描画）
     ctx.drawImage(imgData, HORIZONTAL_MARGIN, HORIZONTAL_MARGIN, imgData.width, imgData.height);
 
-    // テキストを描画（下部の余白に）
-    ctx.fillStyle = '#747474';  // 文字色
-    ctx.font = '400 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;  // フォントの設定
-    // ctx.textAlign = 'center';  // 水平中央揃え
-    ctx.textBaseline = 'middle';  // 垂直中央揃え
-    let textCenter = canvas.height - BOTTOM_MARGIN / 2 // 下の余白の中央位置
+    ctx.fillStyle = '#747474';
+    ctx.font = '400 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;
+    // ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    let textCenter = canvas.height - BOTTOM_MARGIN / 2;
 
-    // 一部のテキストを太字にするための準備
     if ("Make" in exifData) {
       text1 = exifData.Make.replace(/\u0000/g, '');
     }
@@ -194,7 +181,6 @@ window.onload = function () {
     let text2Width = ctx.measureText(text2).width;
     let textStart = canvas.width / 2 - (text1Width + text2Width + ctx.measureText(text3).width) / 2;
 
-    // 2行目のテキスト情報取得
     let exposureTime;
     if (exifData.ExposureTime != undefined) {
       exposureTime = exifData.ExposureTime >= 1 ? exifData.ExposureTime : `1/${Math.round(1 / exifData.ExposureTime)}`;
@@ -208,7 +194,6 @@ window.onload = function () {
     let isoSpeedRatingsText = exifData.ISOSpeedRatings ? `ISO${exifData.ISOSpeedRatings}` : '';
     let finalText = focalLengthText + fNumberText + exposureTimeText + isoSpeedRatingsText;
 
-    // フォームに反映
     makeInput.value = "Make" in exifData ? exifData.Make : '';
     modelInput.value = "Model" in exifData ? exifData.Model : '';
     lensInput.value = "LensModel" in exifData ? exifData.LensModel : '';
@@ -217,24 +202,22 @@ window.onload = function () {
     exposureTimeInput.value = exposureTimeText.replace('s ', '');
     isoSpeedRatingsInput.value = isoSpeedRatingsText.replace('ISO', '');
 
-    // テキスト描画
-    let textHeight = finalText ? textCenter - LINE_SPACING : textCenter + BASE_FONT_SIZE / 2; // 2行目テキストがある場合は上に、ない場合は中央にずらす
-    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;  // フォントの設定を変更
-    ctx.fillStyle = '#000000';  // 文字色
+    let textHeight = finalText ? textCenter - LINE_SPACING : textCenter + BASE_FONT_SIZE / 2;
+    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;
+    ctx.fillStyle = '#000000';
     ctx.fillText(text1, textStart, textHeight);
-    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;  // フォントの設定を変更
-    ctx.fillStyle = '#000000';  // 文字色
+    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;
+    ctx.fillStyle = '#000000';
     ctx.fillText(text2, textStart + text1Width, textHeight);
-    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;  // フォントの設定を戻す
+    ctx.font = '700 ' + BASE_FONT_SIZE + 'px ' + FONT_FAMILY;
     // ctx.fillStyle = '#343434';  // 文字色
     ctx.fillText(text3, textStart + text1Width + text2Width, textHeight);
 
-    ctx.textAlign = 'center';  // 水平中央揃え
-    ctx.font = '400 ' + BASE_FONT_SIZE * 0.8 + 'px ' + FONT_FAMILY;  // フォントの設定
-    ctx.fillStyle = '#747474';  // 文字色
+    ctx.textAlign = 'center';
+    ctx.font = '400 ' + BASE_FONT_SIZE * 0.8 + 'px ' + FONT_FAMILY;
+    ctx.fillStyle = '#747474';
     ctx.fillText(finalText, canvas.width / 2, textCenter + LINE_SPACING + BASE_FONT_SIZE);
 
-    // 画像の描画処理
     let result = canvas.toDataURL('image/jpeg', 1.0);
     if (result === "data:,") {
       toggleLoading(false);
